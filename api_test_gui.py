f55527936787df9e48ba693ebd5f91eb4f5cf5f1
Snychng <snychng@gmail.com>
2023-12-21 13:03:29 +0800

    界面增加进度条，可以自己选择切换模型，增加针对提示词的config文件

--- api_test_gui.py
+++ api_test_gui.py
@@ -3,47 +3,209 @@ from PIL import Image, ImageTk
 from tkinter import ttk
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
-from ttkbootstrap import Style  # Import the Style class
+from ttkbootstrap import Style
 import time
 import threading
+from config import prompts, negative_prompts, ad_prompts, adetailer
 
 # create API client with custom host, port
 api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
 api.set_auth('snychng', '9081726354')
 
+# 全局标志变量，用于控制自动生成的开始和停止
+auto_generate_running = False
+image_count = 0
+start_time = None
+total_time = 0
+progress_stopped = False
+generate_running = False
+
+def start_progress_bar(interval=100):
+    global progress_stopped
+    progress_stopped = False
+    progress_bar['value'] = 0  # 初始化进度条的值为0
+    progress_bar['maximum'] = 100  # 设置进度条的最大值为100
+    progress_bar.stop()  # 确保停止进度条（如果之前在动）
+    
+    def progress():
+        if progress_bar['value'] < 100 and progress_stopped == False:
+            progress_bar['value'] += 1  # 每次调用增加1%
+            root.after(interval, progress)  # 间隔一段时间后再次调用自身
+        elif progress_stopped == False:
+            progress_bar['value'] = 0  # 循环进度条：到达100%后重置为0
+            root.after(interval, progress)  # 继续循环
+        else:
+            progress_bar.stop()
+
+    progress()  # 启动进度条
+
+def stop_progress_bar():
+    global progress_stopped
+    progress_stopped = True
+    progress_bar.stop()  # 停止进度条循环
+    progress_bar['value'] = 0  # 重置进度条的值为0
+    progress_label.config(text="操作完成")  # 更新状态标签
+
+def set_model(event):
+    progress_label.config(text="正在切换模型，请稍等...")
+    start_progress_bar()  # 开始进度条动画
+
+    # 在新线程中切换模型以避免阻塞UI
+    def switch_model_thread():
+        selected_model = model_combobox.get()
+        model_index = models.index(selected_model)
+        api.util_set_model(models[model_index])
+        api.util_wait_for_ready()
+        root.after(0, stop_progress_bar)  # 在主线程中停止进度条
+
+    # 启动新线程来切换模型
+    threading.Thread(target=switch_model_thread).start()
+
 models = api.util_get_model_names()
-# print("Available models:")
-for idx, model in enumerate(models, 1):
-    print(f"{idx}. {model}")
 
-# Set model (find closest match)
-api.util_set_model(models[23])
+def update_time_labels():
+    global start_time, image_count, total_time
+    
+    if start_time is not None:
+        current_time = time.time()
+        total_time = current_time - start_time
+        if image_count > 0:
+            average_time = total_time / image_count
+        else:
+            average_time = 0
 
-# Wait for job to complete
-api.util_wait_for_ready()
+        total_time_label.config(text=f"总共用时：{total_time:.0f}秒")
+        
 
-# Initialize prompts
-prompts = "(intricate details:1.2),(masterpiece:1.3),(best quality:1.4),(ultra high res:1.2),(photography:1.5),HDR,8k resolution,highest quality,detailed and intricate,(real skin texture),<lora:GuoFeng3.2_Lora:0.1>,<lora:fashionGirl_v52:0.1>,<lora:chilloutmixss30_v30:0.15>,<lora:iu_V35:0.15>,<lora:koreanDollLikeness_v15:0.1>,(1girl ),(16yo),(long hair),(skinny:1.31),thin legs,upper body,slender_waist,medium breasts,parted_lips,stirrup_legwear,black_thighhighs,a slender hand,the hand of details,the right hand,"
+    # 每1000毫秒调用一次自身，以更新时间
+    root.after(1000, update_time_labels)
+    
+def generate_image():
+    global image_count, start_time, total_time, progress_stopped, generate_running, auto_generate_running
 
-negative_prompts = "ng_deepnegative_v1_75t,easynegative,paintings,sketches,(worst quality, low quality:1.4),(normal quality:2),lowres,normal quality,((monochrome)),((grayscale)),(duplicate:1.331),(morbid:1.21),(mutilated:1.21),(tranny:1.331),(missing arms:1.331),(extra arms:1.331),no legs,(extra legs:1.331),extra limbs,extra digit,bad hands,bad fingers,extra fingers,(too many fingers:1.61051),missing fingers,deformed fingers,mutated hands,(fused fingers:1.61051),(poorly drawn hands:1.331),(bad anatomy:1.21),bad body,(disfigured:1.331),(bad proportions:1.331),(more than 2 nipples:1.331),skin spots,watermarks,texts,artist name,logo,acnes,skin blemishes,age spot,(outdoor:1.6),manboobs,horror,(fat ass),(bad-artist:0.7),"
+    if not auto_generate_running and generate_running:
+        return  # 如果全局变量表示停止，则立即退出函数
 
-ad_prompts = "(intricate details:1.2),(masterpiece:1.3),(best quality:1.4),(ultra high res:1.2),\
-    (photography:1.5),HDR,8k resolution,highest quality,detailed and intricate,(real skin texture),\
-    <lora:GuoFeng3.2_Lora:0.1>,<lora:fashionGirl_v52:0.1>,<lora:chilloutmixss30_v30:0.15>,\
-    <lora:iu_V35:0.15>,<lora:koreanDollLikeness_v15:0.1>"
+    progress_label.config(text="正在生成图像，请稍等...")  # 设置进度条文本
+    start_progress_bar()  # 开始进度条动画
 
-adetailer = {
-    "ADetailer": {
-      "args": [
-        {
-          "ad_model": "face_yolov8n.pt",
-          "ad_prompt": ad_prompts,
-          "ad_negative_prompt": "",
-          "ad_confidence": 0.3,
-        }
-      ]
-    }
-  }
+    # 在新线程中生成图像以避免阻塞UI
+    def generate_image_thread():
+        global image_count, start_time, total_time
+        if image_count == 0:
+            start_time = time.time()
+
+        # 这里是生成图像的API调用
+        result = api.txt2img(
+            prompt=prompt_text.get('1.0', tk.END),
+            negative_prompt=negative_prompt_text.get('1.0', tk.END),
+            seed=-1,
+            styles=["realistic"],
+            cfg_scale=7,
+            width=512,
+            height=768,
+            sampler_index='DPM++ SDE Karras',
+            steps=28,
+            alwayson_scripts=adetailer,
+        )
+
+        file_path = "./output/tmp.png"
+        result.image.save(file_path)
+
+        def update_ui():
+            global image_count
+            image = Image.open(file_path)
+            tk_image = ImageTk.PhotoImage(image)
+
+            # 更新标签来展示新图像
+            image_label.configure(image=tk_image)
+            image_label.image = tk_image  # Keep a reference
+
+            image_count += 1
+            image_count_label.config(text=f"本次生成的第{image_count}张图像")
+            total_time = time.time() - start_time
+            average_time = total_time / image_count
+            average_time_label.config(text=f"平均每张图片用时：{average_time:.2f}秒")
+
+        # 确保UI更新在主线程中执行
+        root.after(0, stop_progress_bar)
+        root.after(0, update_ui)
+
+    # 启动新线程生成图像
+    threading.Thread(target=generate_image_thread).start()
+
+def single_generate_images():
+    global generate_running
+    generate_running = True
+    generate_image()
+
+def auto_generate_images():
+    global auto_generate_running
+    while auto_generate_running:
+        generate_image()
+        # 这里可以根据需要添加延时
+        time.sleep(15)  # 15秒钟生成一张图像，根据实际情况调整
+        if not auto_generate_running:
+            break  # 如果全局变量表示停止，跳出循环
+
+def start_auto_generate():
+    global auto_generate_running
+    auto_generate_running = True
+    t = threading.Thread(target=auto_generate_images)
+    t.start()
+
+def stop_auto_generate():
+    global auto_generate_running
+    auto_generate_running = False
+
+# 使用更加现代的主题
+style = Style(theme='minty')
+
+# 创建主窗口
+root = style.master
+root.title("SD For Live")
+root.geometry('936x800')  # 设置窗口默认大小
+
+# 定义左侧面板（文本输入）中间面板（图像显示）右侧面板（图像总览）
+control_panel = ttk.Frame(root, padding="10")
+control_panel.grid(row=0, column=0, sticky="nswe")
+view_panel = ttk.Frame(root, padding="10")
+view_panel.grid(row=0, column=1, sticky="nswe")
+
+# 在其他全局变量定义下面添加进度条和状态标签的声明
+progress_label = ttk.Label(root, text="模型状态：未开始", font=("Helvetica", 10))
+progress_bar = ttk.Progressbar(root, mode='indeterminate')
+
+# 配置列权重，使右侧面板相对更宽
+root.grid_columnconfigure(1, weight=3)
+root.grid_rowconfigure(0, weight=1)
+
+# 选择模型
+ttk.Label(control_panel, text="模型列表", font=("Helvetica", 10)).grid(row=0, column=0, sticky="w", pady=2)
+model_combobox = ttk.Combobox(control_panel, values=models, state="readonly")
+model_combobox.grid(row=1, column=0, pady=10, sticky="we")  # Adjust row accordingly
+if len(models) > 22:  # 如果有足够多的模型可供选择
+    model_combobox.current(22)  # 选择第23个模型（索引为22）
+else:
+    model_combobox.current(0)  # 否则，默认选择列表中的第一个模型
+set_model(None)
+stop_progress_bar()
+model_combobox.bind('<<ComboboxSelected>>', set_model)
+
+# 正面提示词的标签和文本框
+ttk.Label(control_panel, text="正面提示词", font=("Helvetica", 10)).grid(row=2, column=0, sticky="w", pady=2)
+prompt_text = ScrolledText(control_panel, height=5, width=50, font=("Helvetica", 10))
+prompt_text.grid(row=3, column=0, pady=2, sticky="we")
+
+# 负面提示词的标签和文本框
+ttk.Label(control_panel, text="负面提示词", font=("Helvetica", 10)).grid(row=4, column=0, sticky="w", pady=2)
+negative_prompt_text = ScrolledText(control_panel, height=5, width=50, font=("Helvetica", 10))
+negative_prompt_text.grid(row=5, column=0, pady=2, sticky="we")
+
+# 面部提示词的标签和文本框
+ttk.Label(control_panel, text="面部提示词", font=("Helvetica", 10)).grid(row=6, column=0, sticky="w", pady=2)
+ad_prompt_text = ScrolledText(control_panel, height=5, width=50, font=("Helvetica", 10))
+ad_prompt_text.grid(row=7, column=0, pady=2, sticky="we")
 
 # 修改add_placeholder函数内的颜色设置以符合默认状态
 def add_placeholder(text_widget, placeholder):
@@ -63,153 +225,46 @@ def add_placeholder(text_widget, placeholder):
     text_widget.bind("<FocusIn>", on_focusin)
     text_widget.bind("<FocusOut>", on_focusout)
 
-def update_time_labels():
-    global start_time, image_count, total_time
-    
-    if start_time is not None:
-        current_time = time.time()
-        total_time = current_time - start_time
-        if image_count > 0:
-            average_time = total_time / image_count
-        else:
-            average_time = 0
-
-        total_time_label.config(text=f"总共用时：{total_time:.0f}秒")
-        
-
-    # 每1000毫秒调用一次自身，以更新时间
-    root.after(1000, update_time_labels)
-
-def generate_image():
-    global image_count, start_time, total_time
-
-    # 如果是第一张图片，记录开始时间
-    if image_count == 0:
-        start_time = time.time()
-
-    result = api.txt2img(
-        prompt=prompt_text.get('1.0', tk.END),
-        negative_prompt=negative_prompt_text.get('1.0', tk.END),
-        seed=-1,
-        styles=["realistic"],
-        cfg_scale=7,
-        width=512,
-        height=768,
-        sampler_index='DPM++ SDE Karras',
-        steps=28,
-        alwayson_scripts=adetailer,
-    )
-    
-    file_path = "D:/SD4Live/output/tmp.png"
-    result.image.save(file_path)
-    
-    # 加载图片并转换为Tkinter可用的格式
-    image = Image.open(file_path)
-    tk_image = ImageTk.PhotoImage(image)
-
-    # 更新标签来展示新图片
-    image_label.configure(image=tk_image)
-    image_label.image = tk_image  # Keep a reference
-
-    # 图像计数增加并更新标签
-    image_count += 1
-    image_count_label.config(text=f"本次生成的第{image_count}张图像")
-    total_time = time.time() - start_time
-    average_time = total_time / image_count
-    average_time_label.config(text=f"平均每张图片用时：{average_time:.2f}秒")
-
-# 全局标志变量，用于控制自动生成的开始和停止
-auto_generate_running = False
-image_count = 0
-start_time = None
-total_time = 0
-
-def auto_generate_images():
-    global auto_generate_running
-    while auto_generate_running:
-        generate_image()
-        # 这里可以根据需要添加延时
-        time.sleep(1)  # 1秒钟生成一张图像，根据实际情况调整
-
-def start_auto_generate():
-    global auto_generate_running
-    auto_generate_running = True
-    t = threading.Thread(target=auto_generate_images)
-    t.start()
-
-def stop_auto_generate():
-    global auto_generate_running
-    auto_generate_running = False
-
-
-# ... 其余的代码保持不变 ...
-
-# 使用更加现代的主题
-style = Style(theme='minty')
-
-# 创建主窗口
-root = style.master
-root.title("AI Image Generator")
-root.geometry('936x800')  # 设置窗口默认大小
-
-# 定义左侧面板（文本输入）和右侧面板（图像显示）
-left_panel = ttk.Frame(root, padding="10")
-left_panel.grid(row=0, column=0, sticky="nswe")
-right_panel = ttk.Frame(root, padding="10")
-right_panel.grid(row=0, column=1, sticky="nswe")
-
-# 配置列权重，使右侧面板相对更宽
-root.grid_columnconfigure(1, weight=3)
-root.grid_rowconfigure(0, weight=1)
-
-# 使用 Grid 布局替代 Pack
-def add_labeled_textbox(parent, label, row, font=("Helvetica", 10)):
-    ttk.Label(parent, text=label, font=font).grid(row=row, column=0, sticky="w", pady=2)
-    text_box = ScrolledText(parent, height=5, width=50, font=font)
-    text_box.grid(row=row + 1, column=0, pady=2, sticky="we")
-    return text_box
-
-# 输入框和它们的提示
-prompt_text = add_labeled_textbox(left_panel, "正面提示词", 0)
-negative_prompt_text = add_labeled_textbox(left_panel, "负面提示词", 2)
-ad_prompt_text = add_labeled_textbox(left_panel, "面部提示词", 4)
-
 # 设置默认提示词
 add_placeholder(prompt_text, prompts)
 add_placeholder(negative_prompt_text, negative_prompts)
 
 # 添加按钮
-generate_button = ttk.Button(left_panel, text="生成图像")
-generate_button.grid(row=6, column=0, pady=10, sticky="we")
-start_auto_generate_button = ttk.Button(left_panel, text="开始自动生成", command=start_auto_generate)
-start_auto_generate_button.grid(row=7, column=0, pady=10, sticky="we")
-stop_auto_generate_button = ttk.Button(left_panel, text="停止自动生成", command=stop_auto_generate)
-stop_auto_generate_button.grid(row=8, column=0, pady=10, sticky="we")
+generate_button = ttk.Button(control_panel, text="生成图像", command=generate_image)
+generate_button.grid(row=8, column=0, pady=10, sticky="we")
+start_auto_generate_button = ttk.Button(control_panel, text="开始自动生成", command=start_auto_generate)
+start_auto_generate_button.grid(row=9, column=0, pady=10, sticky="we")
+stop_auto_generate_button = ttk.Button(control_panel, text="停止自动生成", command=stop_auto_generate)
+stop_auto_generate_button.grid(row=10, column=0, pady=10, sticky="we")
 
 # 在UI中添加显示图像计数的标签
-image_count_label = ttk.Label(right_panel, text="本次生成的第0张图像")
-image_count_label.grid(row=1, column=0, pady=10, sticky="nswe")
+image_count_label = ttk.Label(view_panel, text="本次生成的第0张图像")
+image_count_label.grid(row=1, column=0, pady=10, sticky="w")  # 左对齐
 
-total_time_label = ttk.Label(right_panel, text="总共用时：0.00秒")
-total_time_label.grid(row=2, column=0, pady=10, sticky="nswe")
+total_time_label = ttk.Label(view_panel, text="本次生成总用时：0秒")
+total_time_label.grid(row=1, column=1, pady=10)  # 居中对齐（默认，不需要 "nswe"）
 
-average_time_label = ttk.Label(right_panel, text="平均每张图片用时：0.00秒")
-average_time_label.grid(row=3, column=0, pady=10, sticky="nswe")
+average_time_label = ttk.Label(view_panel, text="平均每张图片用时：0.00秒")
+average_time_label.grid(row=1, column=2, pady=10, sticky="e")  # 右对齐
 
 # 美化图像显示区域
 default_image = Image.new('RGB', (512, 768), 'white')  # 创建一个默认白色图片
 default_photo = ImageTk.PhotoImage(default_image)
-image_label = ttk.Label(right_panel, image=default_photo, relief="solid", borderwidth=1)
-image_label.grid(row=0, column=0, pady=10, sticky="nswe")
+image_label = ttk.Label(view_panel, image=default_photo, relief="solid", borderwidth=1)
+image_label.grid(row=0, column=0, columnspan=3, pady=10, sticky="nswe")
 
 # 保持图像的宽高比
-right_panel.grid_rowconfigure(0, weight=1)
-right_panel.grid_columnconfigure(0, weight=1)
+view_panel.grid_rowconfigure(0, weight=1)
+view_panel.grid_columnconfigure(0, weight=1)
+
+# 在布局配置的最后添加进度条和标签
+progress_label = ttk.Label(root, text="操作状态：未开始", font=("Helvetica", 10))
+progress_bar = ttk.Progressbar(root, mode='indeterminate')
+progress_label.grid(row=2, column=0, columnspan=2, sticky='ew', padx=10, pady=5)
+progress_bar.grid(row=3, column=0, columnspan=2, sticky='ew', padx=10, pady=5)
 
 # 配置按钮命令
 generate_button.configure(command=generate_image)
-
-# ... 其余代码不变 ...
 
 # 启动定时更新时间标签的函数
 update_time_labels()
